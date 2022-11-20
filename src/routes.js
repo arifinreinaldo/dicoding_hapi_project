a2d576f35ca2b9aafc3ac5891730b491db615f0b
DESKTOP-L81CEII\Reinaldo <arifin.reinaldo@gmail.com>
2022-11-20 18:04:27 +0800

    add initial project

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+// const {addNotesHandler, getAllNoteHandler, getNoteHandler, updateNoteHandler,deleteNoteHandler} = require('./handler');
+
+const routes = [{
+    method: 'GET', path: '/', handler: () => ('gororo'),
+}/*, {
+    method: 'GET', path: '/about', handler: () => ('aboot'),
+}, {
+    method: 'GET', path: '/gorom', handler: () => ('Uknown'),
+}, {
+    method: 'GET', path: '/notes', handler: () => getAllNoteHandler(),
+}, {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteHandler,
+}, {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: updateNoteHandler,
+}, {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteHandler,
+}, {
+    method: 'POST',
+    path: '/notes',
+    handler: addNotesHandler,
+    options: {
+        cors: {
+            origin: ['*'],
+        },
+    },
+}*/];
+
+module.exports = routes;
